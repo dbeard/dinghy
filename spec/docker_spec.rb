f424c88b9bcb2f3f9f1c01f5506742253d338e21
Danny Beard <dbeard@arthrex.com>
2016-03-01 15:53:59 -0500

    Fix failing test

--- spec/docker_spec.rb
+++ spec/docker_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Docker do
     it 'preserves the original environment' do
       expect(Kernel).to receive(:system).with("docker", "ps").and_return "zero"
       ENV['DOCKER_HOST'] = 'myhost'
-      expect(subject.system("ps")).to eq('zero')
+      expect(subject.exec("ps")).to eq('zero')
       expect(ENV['DOCKER_HOST']).to eq('myhost')
     end
   end
